14af17532cd55d611976f942c67c57c7700af176
GhostWilly12k <willysbuda@gmail.com>
2025-04-09 23:38:15 +0200

    firebase config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
-const admin = require("firebase-admin");
+const express = require('express');
+const admin = require('firebase-admin');
+const app = express();
+
+const serviceAccount = require('./serviceAccountKey.json');
+
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https://testApp.firebaseio.com"
-});
+    credential: admin.credential.cert(serviceAccount)
+  });
+  
+  const db = admin.firestore();
+  
+  app.use(express.json());
+
+  module.exports = app;
